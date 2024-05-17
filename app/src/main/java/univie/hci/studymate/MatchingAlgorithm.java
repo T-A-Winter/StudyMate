82ce0f607dcaad6946f0bdadeab9d6626639e82e
Tobias Winter <tobias.a.winter@gmail.com>
2024-05-17 14:21:22 +0200

    Matching Algo now is inited with 20 random users

--- app/src/main/java/univie/hci/studymate/MatchingAlgorithm.java
+++ app/src/main/java/univie/hci/studymate/MatchingAlgorithm.java
@@ -8,7 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class MatchingAlgorithm extends AppCompatActivity {
+    Random random = new Random();
+    // matching algo is created with random users
+    Matching matchingAlgo = new Matching(createRandomUsers());
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,27 @@ public class MatchingAlgorithm extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private Collection<User> createRandomUsers() {
+        // creating the users
+        return IntStream.range(1, 21)
+                .mapToObj(userIndex -> {
+                    String name = "User" + userIndex;
+                    String email = "user" + userIndex + "@example.com";
+
+                    // rand Uni
+                    University randomUniversity = University.values()[random.nextInt(University.values().length)];
+
+                    // selecting 1-3 rand Tags
+                    List<Tag> tagList = random.ints(1,4)
+                            .mapToObj(n -> Tag.values()[random.nextInt(Tag.values().length)])
+                            .distinct()
+                            .limit(3)
+                            .collect(Collectors.toList());
+
+                    return new User(name, randomUniversity, tagList, email);
+                })
+                .collect(Collectors.toList());
     }
 }
