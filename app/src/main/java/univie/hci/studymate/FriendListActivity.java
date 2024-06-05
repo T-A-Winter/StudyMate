6536f4a27463da63c423dc0581b394669192dd83
Tobias Winter <tobias.a.winter@gmail.com>
2024-06-05 17:10:09 +0200

    NavBar now working in FriendListActivity

--- app/src/main/java/univie/hci/studymate/FriendListActivity.java
+++ app/src/main/java/univie/hci/studymate/FriendListActivity.java
@@ -3,15 +3,24 @@ package univie.hci.studymate;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class FriendListActivity extends AppCompatActivity {
@@ -21,6 +30,9 @@ public class FriendListActivity extends AppCompatActivity {
     private int currentBackgroundIndex = 0;
     private RecyclerView mainLayout;
     private RelativeLayout toolBar;
+    static final private String USER_MATCHING_ALGO_STRING = MainActivity.USER_MATCHING_ALGO_STRING;
+    private User user;
+    private NavBar navBar;
 
     private int[] backgroundResources = {
             R.drawable.background_gradient,
@@ -58,11 +70,44 @@ public class FriendListActivity extends AppCompatActivity {
         });
         currentBackgroundIndex = getSharedPreferences("prefs", MODE_PRIVATE).getInt("backgroundIndex", 0);
         applyBackground();
+
+        // setting up Navbar
+        user = getUserFromIntent();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        navBar = new NavBar(this, bottomNavigationView, user);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_navigation_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return navBar.onMenuItemClick(item) || super.onOptionsItemSelected(item);
     }
 
     private void applyBackground() {
         Drawable background = ContextCompat.getDrawable(this, backgroundResources[currentBackgroundIndex]);
         mainLayout.setBackground(background);
         toolBar.setBackground(background);
+    }
+
+    private User getUserFromIntent() {
+        Intent intent = getIntent();
+        User user = intent.getParcelableExtra(USER_MATCHING_ALGO_STRING);
+        if (user == null) {
+            user = setFailSafeUser();
+        }
+        return user;
+    }
+
+    private User setFailSafeUser() {
+        String name = "failSafeUser";
+        Collection<Tag> tags = new ArrayList<>(Arrays.asList(Tag.ERSTI, Tag.HCI));
+        University uni = University.UNI_WIEN;
+        String email = "failsafe@example.com";
+        return new User(name, uni, tags, email);
     }
 }
